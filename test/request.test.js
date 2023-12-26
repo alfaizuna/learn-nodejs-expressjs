b24a4e2edd3b177bd223d1e8d786e9e23f43136b
alfaizuna <alfaizunaulia@gmail.com>
2023-12-26 22:29:56 +0700

    super test

---
+++ test/request.test.js
@@ -0,0 +1,16 @@
+const request = require("supertest");
+const express = require('express');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+test("Test ExpressJS", async () => {
+   const response = await request(app).get("/");
+   expect(response.text).toBe("Hello, Express!");
+});
+
+
