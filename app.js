0995ecfde113220f865dcf68cb2003e3c81f79bb
alfaizuna <alfaizunaulia@gmail.com>
2023-12-26 12:05:28 +0700

    hello alfaizuna

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello, Express!');
 });
 
+app.get('/alfaizuna', (req, res) => {
+    res.send('Hello, Alfaizuna!');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
